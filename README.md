0bba9cde99e03e697c423a38d4bba1db484dafe1
Matheus Henrique <nilet@tutanota.com>
2023-03-14 11:10:52 -0300

    route for file and for json, changed names, added dependencies

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-Instalar dependencias:
+### Instalar dependencias:
 ```pip install -r requirements.txt```
 
-Rode localmente para desenvolvimento com: 
+### Rode localmente para desenvolvimento com: 
 ```uvicorn main:app --reload```
 
-Rode em producao com:
+### Rode em producao com:
 ## A definir com a parte do heroku
+
+### Documentacao (a fazer) e listar rotas
+[Listar Rotas rodando localmente](http://127.0.0.1:8000/docs#/)
+
 
--- main.py
+++ main.py
@@ -1,18 +1,48 @@
-from typing import Union
-from fastapi import FastAPI, File, UploadFile
+from io import StringIO
+from typing import Union, Optional
+from fastapi import FastAPI, Request, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 import pandas as pd
+import random
 
-app = FastAPI()
+app = FastAPI(title="Lidio-O-Vigilante-Api666")
 
-@app.post("/csv2xlsx")
-async def csv2xlsx(file:UploadFile):
-    if not file.filename.endswith(".csv"):
-        return { "error": "Not a csv file"}
+class CSVRequest(BaseModel):
+    body: str
 
-    csv_file = pd.read_csv(file.file)
-    resultadoExcel = pd.ExcelWriter(file.filename.replace(".csv", ".xlsx"), engine='xlsxwriter')
-    csv_file.to_excel(resultadoExcel, index=None)
-    resultadoExcel.save()
+@app.post("/fileCsvToXlsx")
+async def fileCsvToXlsx(file: Union[UploadFile, None] = None, json_data: Union[dict, None] = None):
+    if json_data is not None:
+        try:
+            print(json_data)
+            return {"csv_received": True}
+        except Exception as e:
+            raise HTTPException(status_code=422, detail=str(e))
 
-    return FileResponse(file.filename.replace(".csv", ".xlsx"))
+    elif file is not None:
+        if not file.filename.endswith(".csv"):
+            return { "error": "Not a csv file"}
+        else:
+            csv_file = pd.read_csv(file.file)
+            resultadoExcel = pd.ExcelWriter(file.filename.replace(".csv", ".xlsx"), engine='xlsxwriter')
+            csv_file.to_excel(resultadoExcel, index=None)
+            resultadoExcel.save()
+
+            return FileResponse(file.filename.replace(".csv", ".xlsx"))
+
+@app.post("/jsonCsvToXlsx")
+async def jsonCsvToXlsx(csv_request: CSVRequest):
+    try:
+        csvString = csv_request.body
+        csvStringIO = StringIO(csvString)
+        csv_file = pd.read_csv(csvStringIO)
+        filename = 'Roi'+ str(random.randint(100000000, 999999999)) + ".xlsx"
+        resultadoExcel = pd.ExcelWriter(filename, engine='xlsxwriter')
+        csv_file.to_excel(resultadoExcel, index=None)
+        resultadoExcel.save()
+        return FileResponse(filename)
+
+
+    except Exception as e:
+        raise HTTPException(status_code=422, detail=str(e))
