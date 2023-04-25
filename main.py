84c8bb9a9d3fecb656b5ed3f697e2b585ff469b2
Nilet <nilet@tutanota.com>
2023-04-25 11:20:39 -0300

    commented auth, fixed excelwriter

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import pandas as pd
 import random
 import io
 
-TOKEN_FILE = "token.txt"
+# TOKEN_FILE = "token.txt"
 
 app = FastAPI(title="Lidio-O-Vigilante-Api666")
 
 
-def authenticate(token: str):
-    with open(TOKEN_FILE, "r") as f:
-        saved_token = f.read().strip()
-        if token == saved_token:
-            return True
-    return False
+# def authenticate(token: str):
+#     with open(TOKEN_FILE, "r") as f:
+#         saved_token = f.read().strip()
+#         if token == saved_token:
+#             return True
+#     return False
 
 class CSVRequest(BaseModel):
     body: str
 
 @app.post("/fileCsvToXlsx")
 async def fileCsvToXlsx(file:UploadFile, token: str = Header(None)):
-    if not authenticate(token):
-        raise HTTPException(status_code=401, detail="Invalid token")
+    # if not authenticate(token):
+    #     raise HTTPException(status_code=401, detail="Invalid token")
 
     if not file.filename.endswith(".csv"):
         return { "error": "Not a csv file"}
@@ -33,14 +33,14 @@ async def fileCsvToXlsx(file:UploadFile, token: str = Header(None)):
     csv_file = pd.read_csv(file.file)
     resultadoExcel = pd.ExcelWriter(file.filename.replace(".csv", ".xlsx"), engine='xlsxwriter')
     csv_file.to_excel(resultadoExcel, index=None)
-    resultadoExcel.save()
+    resultadoExcel.close()
 
     return FileResponse(file.filename.replace(".csv", ".xlsx"))
 
 @app.post("/jsonCsvToXlsx")
 async def jsonCsvToXlsx(csv_request: CSVRequest, token: str = Header(None)):
-    if not authenticate(token):
-        raise HTTPException(status_code=401, detail="Invalid token")
+    # if not authenticate(token):
+    #     raise HTTPException(status_code=401, detail="Invalid token")
 
     try:
         csvString = csv_request.body
@@ -49,7 +49,7 @@ async def jsonCsvToXlsx(csv_request: CSVRequest, token: str = Header(None)):
         filename = 'Roi'+ str(random.randint(100000000, 999999999)) + ".xlsx"
         resultadoExcel = pd.ExcelWriter(filename, engine='xlsxwriter')
         csv_file.to_excel(resultadoExcel, index=None)
-        resultadoExcel.save()
+        resultadoExcel.close()
         return FileResponse(filename)
 
 
